35f2f9966b1daab14f7dfbb9e6e14e2f19c153ca
Sean McKenna <mckennapsean@gmail.com>
2013-09-02 17:39:19 -0600

    add object DOM loop, needs to check ray intersections

--- ray-tracer.cpp
+++ ray-tracer.cpp
@@ -23,19 +23,74 @@
 using namespace std;
 
 
+// variables for ray tracing
+int w;
+int h;
+int size;
+Color24* img;
+
+
+// variables for camera ray generation
+void cameraRayVars();
+Point3 *imageTopLeftV;
+Point3 *dXV;
+Point3 *dYV;
+Point3 firstPixel;
+Point3 cameraRay(int pX, int pY);
+
+
+// trace a ray against all objects
+void objectIntersection(Node &n, Ray r);
+
+
 // ray tracer
 int main(){
   
   // load scene: root node, camera, image
   LoadScene("scenes/prj0.xml");
   
-  // variables for generating camera rays
-  int w = renderImage.GetWidth();
-  int h = renderImage.GetHeight();
-  int size = w * h;
+  // variables for ray tracing
+  w = renderImage.GetWidth();
+  h = renderImage.GetHeight();
+  size = w * h;
   Color24 white = {237, 237, 237};
   Color24 black = {27, 27, 27};
-  Color24* img = renderImage.GetPixels();
+  img = renderImage.GetPixels();
+  
+  // variables for generating camera rays
+  cameraRayVars();
+  
+  // ray-tracing loop, per pixel
+  for(int i = 0; i < size; i++){
+    
+    // establish pixel location
+    int pX = i % w;
+    int pY = i / w;
+    
+    // compute current ray (in camera space)
+    Point3 rayPos = camera.pos;
+    Point3 rayDir = cameraRay(pX, pY);
+    
+    // transform ray into world space
+    Transformation* world = new Transformation();
+    world->Translate(rayPos);
+    rayDir=world->VectorTransformFrom(rayDir);
+    Ray *curr = new Ray(rayPos, rayDir);
+    
+    // traverse through scene DOM
+    // transform rays into model space
+    // detect ray intersections ---> update pixel
+    objectIntersection(rootNode, *curr);
+  }
+  
+  // output ray-traced image & z-buffer
+  //renderImage.SaveImage("images/image.ppm");
+  //renderImage.SaveZImage("images/z-image.ppm");
+}
+
+
+// create variables for camera ray generation
+void cameraRayVars(){
   float fov = camera.fov;
   float aspectRatio = (float) w / (float) h;
   float imageDistance = 1;
@@ -43,30 +98,40 @@ int main(){
   float imageTipX = imageTipY * aspectRatio;
   float dX = (2.0 * imageTipX) / w;
   float dY = (2.0 * imageTipY) / h;
-  Point3 *imageTopLeftV = new Point3(-imageTipX, imageTipY, -imageDistance);
-  Point3 *dXV = new Point3(dX, 0.0, 0.0);
-  Point3 *dYV = new Point3(0.0, -dY, 0.0);
-  Point3 firstPixel = *imageTopLeftV + (*dXV * 0.5) + (*dYV * 0.5);
+  imageTopLeftV = new Point3(-imageTipX, imageTipY, -imageDistance);
+  dXV = new Point3(dX, 0.0, 0.0);
+  dYV = new Point3(0.0, -dY, 0.0);
+  firstPixel = *imageTopLeftV + (*dXV * 0.5) + (*dYV * 0.5);
+}
+
+
+// compute camera rays
+Point3 cameraRay(int pX, int pY){
+ Point3 ray = firstPixel + (*dXV * pX) + (*dYV * pY);
+ ray.Normalize();
+ return ray;
+}
+
+
+// recursive object intersection through all scene objects
+void objectIntersection(Node &n, Ray r){
   
-  // generate camera rays for every pixel
-  for(int i = 0; i < size; i++){
+  // loop on child nodes
+  int i = 0;
+  int numChild = n.GetNumChild();
+  while(i < numChild){
     
-    // generate current ray in camera space
-    int pX = i % w;
-    int pY = i / w;
-    Point3 curr = firstPixel + (*dXV * pX) + (*dYV * pY);
-    curr.Normalize();
+    // grab child node
+    Node *child = n.GetChild(i);
     
-    // transform ray into world space
+    // transform rays into model space (or local space)
     
     
-    // traverse through scene DOM
-    // transform rays into model space
-    // detect ray intersections ---> update pixel
+    // compute ray intersections
     
+    
+    // recursively check this child's children
+    objectIntersection(*child, r);
+    i++;
   }
-  
-  // output ray-traced image & z-buffer
-  //renderImage.SaveImage("images/image.ppm");
-  //renderImage.SaveZImage("images/z-image.ppm");
 }
