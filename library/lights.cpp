0a0d0ad102ebbd212aea45e207b00dc930d73283
Sean McKenna <mckennapsean@gmail.com>
2013-09-07 23:34:01 -0600

    add light class, including ambient, direct, and point lights

---
+++ library/lights.cpp
@@ -0,0 +1,142 @@
+// Copyright 2013 Sean McKenna
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+
+// light sub-classes (e.g. generic, ambient, direct, point)
+
+
+// namespace
+using namespace scene;
+namespace scene{
+
+
+// generic light definition (from main light class)
+class GenericLight: public Light{};
+
+
+// ambient light definition
+class AmbientLight: public GenericLight{
+  public:
+    
+    // constructor
+    AmbientLight(){
+      intensity.Set(0, 0, 0);
+    }
+    
+    // get color of ambient light
+    Color illuminate(Point p){
+      return intensity;
+    }
+    
+    // get direction of ambient light (non-sensical)
+    Point direction(Point p){
+      return Point(0, 0, 0);
+    }
+    
+    // return true, since light is ambient
+    bool isAmbient(){
+      return true;
+    }
+    
+    // set color of ambient light
+    void setIntensity(Color c){
+      intensity = c;
+    }
+    
+  private:
+    
+    // intensity (or color) of light
+    Color intensity;
+};
+
+
+// direct light definition
+class DirectLight: public GenericLight{
+  public:
+    
+    // constructor
+    DirectLight(){
+      intensity.Set(0, 0, 0);
+      direction.Set(0, 0, 1);
+    }
+    
+    // get color of direct light
+    Color illuminate(Point p){
+      return intensity;
+    }
+    
+    // get direction of direct light (constant)
+    Point direction(Point p){
+      return direction;
+    }
+    
+    // set color of direct light
+    void setIntensity(Color c){
+      intensity = c;
+    }
+    
+    // set direction of direct light
+    void setDirection(Point dir){
+      direction = dir.GetNormalized();
+    }
+    
+  private:
+    
+    // intensity (or color) of light
+    Color intensity;
+    
+    // direction of light
+    Point direction;
+};
+
+
+// point light definition
+class PointLight: public GenericLight{
+  public:
+    
+    // constructor
+    PointLight(){
+      intensity.Set(0, 0, 0);
+      position.Set(0, 0, 0);
+    }
+    
+    // get color of point light
+    Color illuminate(Point p){
+      return intensity;
+    }
+    
+    // get direction of point light (calculated)
+    Point direction(Point p){
+      return (p - position).GetNormalized();
+    }
+    
+    // set color of point light
+    void setIntensity(Color c){
+      intensity = c;
+    }
+    
+    // set the location of the point light
+    void setPosition(Point pos){
+      position = pos;
+    }
+    
+  private:
+    
+    // intensity (or color) of light
+    Color intensity;
+    
+    // location of light
+    Point position;
+};
+}
