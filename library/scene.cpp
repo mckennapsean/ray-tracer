cc840dccc43514432cf00fa9aac6bc49c27fe75b
Sean McKenna <mckennapsean@gmail.com>
2013-10-18 20:11:39 -0400

    update scene file with appropriate changes for texturing

--- library/scene.cpp
+++ library/scene.cpp
@@ -38,6 +38,7 @@ typedef unsigned char uchar;
 #define min(a, b) ((a) < (b) ? (a):(b))
 #define max(a, b) ((a) > (b) ? (a):(b))
 #define FLOAT_MAX 1.0e30f
+#define TEXTURE_SAMPLE_COUNT 32
 
 
 // declare namespace
@@ -59,10 +60,61 @@ class Ray{
       pos = p;
       dir = d;
     }
+    Ray(Ray &r){
+      pos = r.pos;
+      dir = r.dir;
+    }
     
     // normalize the ray (only the direction is necessary)
     void normalize(){
       dir.Normalize();
+    }
+};
+
+
+// add a cone to a ray for casting and differential textures (anti-aliasing textures)
+class Cone: public Ray{
+  public:
+    
+    // tangent of the cone angle & cone radius at origin
+    float tan, radius;
+    
+    // cone ray constructor
+    Cone(){}
+    Cone(Point &p, Point &d, float t = 0.0, float r = 0.0){
+      pos = p;
+      dir = d;
+      tan = t;
+      radius = r;
+    }
+    Cone(Ray &r, float t = 0.0, float r = 0.0){
+      pos = r.pos;
+      dir = r.dir;
+      tan = t;
+      radius = r;
+    }
+    Cone(Cone &c){
+      pos = c.pos;
+      dir = c.dir;
+      tan = c.tan;
+      radius = c.radius;
+    }
+    
+    // returns a radius for some paramemter, t
+    float radiusAt(float t){
+      return (t * tan + radius) * dir.Length();
+    }
+    
+    // returns the major & minor axes of an ellipse for for some given value
+    void ellipseAt(float t, Point &n, Point &major, Point &minor){
+      float r = radiusAt(t);
+      Point d = dir.getNormalized();
+      Point T = (d ^ N).GetNormalized();
+      minor = r * T;
+      float c = fabs(d % N);
+      if(c < 0.01)
+        c = 0.01;
+      major = (r / c) * (T ^ N).GetNormalized();
     }
 };
 
@@ -256,6 +308,10 @@ struct HitInfo{
   // surface normal of the object at the hit point
   Point n;
   
+  // texture coordinates & derivatives of texture coordinates
+  Point uvw;
+  Point duvw[2];
+  
   // object node that ray hits
   Node *node;
   
@@ -270,6 +326,9 @@ struct HitInfo{
   // initialize hit info
   void init(){
     z = FLOAT_MAX;
+    uvw.Set(0.5, 0.5, 0.5);
+    duvw[0].Zero();
+    duvw[1].Zero();
     node = NULL;
     front = true;
   }
@@ -509,8 +568,8 @@ class Object{
   public:
     virtual ~Object() = 0;
     
-    // intersect ray function for each object
-    virtual bool intersectRay(Ray &r, HitInfo &h, int face = HIT_FRONT) = 0;
+    // intersect ray function for each object (along with ray cones)
+    virtual bool intersectRay(Cone &r, HitInfo &h, int face = HIT_FRONT) = 0;
     
     // bounding box function for each object
     virtual BoundingBox getBoundBox() = 0;
@@ -547,9 +606,9 @@ class Material: public ItemBase{
   public:
     
     // shade method which calls all lights in the list
-    // uses the incoming ray, hit info of rendering pixel, and all lights
+    // uses the incoming ray or cone, hit info of rendering pixel, and all lights
     // also keeps an integer count of how many reflection bounces remaining
-    virtual Color shade(Ray &r, HitInfo &h, LightList &lights, int bounceCount = 1) = 0;
+    virtual Color shade(Cone &r, HitInfo &h, LightList &lights, int bounceCount = 1) = 0;
 };
 
 
@@ -562,6 +621,185 @@ class MaterialList: public ItemList<Material> {
         if(at(i) && name == at(i)->getName())
           return at(i);
       return NULL;
+    }
+};
+
+
+// Texture definition (how to store textures)
+class Texture: public ItemBase{
+  public:
+    
+    // evaluate the color at some given uvw location
+    virtual Color sample(Point &uvw) = 0;
+    
+    // evaluate the color using derivatives recursively
+    virtual Color sample(Point &uvw, Point duvw[2], bool elliptic = true){
+      
+      // begin sampling the texture
+      Color c = sample(uvw);
+      
+      // stop re-sampling the texture when good enough
+      if(duvw[0].LengthSquared() + duvw[1].LengthSquared() == 0)
+        return c;
+      
+      // continue the re-sampling the texture recursively
+      for(int i = 0; i < TEXTURE_SAMPLE_COUNT; i++){
+        
+        // set variables
+        float x = 0;
+        float y = 0;
+        float fx = 0.5;
+        float fy = 1.0 / 3.0;
+        
+        // Halton sequence, base 2 & 3
+        for(int ix = i; ix > 0; ix /= 2){
+          x += fx * (ix % 2);
+          fx /= 2;
+        }
+        for(int iy = i; iy > 0; iy /= 3){
+          y += fy * (iy % 3);
+          fy /= 3;
+        }
+        
+        // elliptic texture sampling (cone)
+        if(elliptic){
+          float r = sqrtf(x) * 0.5;
+          x = r * sinf(y * (float) M_PI * 2.0);
+          y = r * cosf(y * (float) M_PI * 2.0);
+        
+        // for non-elliptic sampling (no cones)
+        }else{
+          if(x > 0.5)
+            x -= 1.0;
+          if(y > 0.5)
+            y -= 1.0;
+        }
+        
+        // continue re-sampling the texture
+        c += sample(uvw + x * duvw[0] + y * duvw[1]);
+      }
+      
+      // return the sampled texture color
+      return c / float(TEXTURE_SAMPLE_COUNT);
+    }
+    
+  protected:
+    
+    // clamps the uvw points for textures that tile (between 0 & 1)
+    Point tileClamp(Point &uvw){
+      Point u;
+      u.x = uvw.x - (int) uvw.x;
+      u.y = uvw.y - (int) uvw.y;
+      u.z = uvw.z - (int) uvw.z;
+      if(u.x < 0.0)
+        u.x += 1.0;
+      if(u.y < 0.0)
+        u.y += 1.0;
+      if(u.z < 0.0)
+        u.z += 1.0;
+      return u;
+    }
+};
+
+
+// ItemFileList for textures definition (stores all textures)
+typedef ItemFileList<Texture> TextureList;
+
+
+// TextureMap definition (handles textures that need transformations)
+// uvw sampled values are always first transformed into texture space
+class TextureMap: public Transformation{
+  private:
+    
+    // variables
+    Texture *texture;
+  
+  public:
+    
+    // constructors
+    TextureMap(){
+      texture = NULL;
+    }
+    TextureMap(Texture *t){
+      texture = t;
+    }
+    
+    // set texture
+    void setTexture(Texture *t){
+      texture = t;
+    }
+    
+    // sample texture (with and without derivatives)
+    virtual Color sample(Point &uvw){
+      return texture ? texture->sample(transformTo(uvw)): Color(0.0, 0.0, 0.0);
+    }
+    virtual Color sample(Point &uvw, Point duvw[2], bool elliptic = true){
+      if(texture == NULL)
+        return Color(0.0, 0.0, 0.0);
+      Point u = transformTo(uvw);
+      Point d[2];
+      d[0] = transformTo(duvw[0] + uvw) - u;
+      d[1] = transformTo(duvw[1] + uvw) - u;
+      return texture->sample(u, d, elliptic);
+    }
+};
+
+
+// TexturedColor definition (stores a texturemap & color
+// will multiply texture with color if needed, too
+class TexturedColor{
+  private:
+    
+    // variables
+    Color *color;
+    TextureMap *map;
+  
+  public:
+    
+    // constructors
+    TexturedColor(){
+      color = Color(0.0, 0.0, 0.0);
+      map = NULL;
+    }
+    TexturedColor(float r, float g, float b){
+      color = Color(r, g, b);
+      map = NULL;
+    }
+    
+    // set color
+    void setColor(Color &c){
+      color = c;
+    }
+    
+    // set texture map
+    void setTexture(TextureMap *m){
+      map = m;
+    }
+    
+    // get the current color
+    Color getColor(){
+      return color;
+    }
+    
+    // get the current texture map
+    TextureMap* getTexture(){
+      return map;
+    }
+    
+    // sample texture (with and without derivatives)
+    Color sample(Point &uvw){
+      return map ? color * map->sample(uvw): color;
+    }
+    Color sample(Point &uvw, Point duvw[2], bool elliptic = true){
+      return map ? color * map->sample(uvw, duvw, elliptic): color;
+    }
+    
+    // return the appropriate color of the texture for environment mapping
+    Color sampleEnvironment(Point &dir){
+      float z = asinf(-dir.z) / float(M_PI) + 0.5;
+      float x = dir.x / (fabs(dir.x) + fabs(dir.y));
+      float y = dir.y / (fabs(dir.x) + fabs(dir.y));
+      return sample(Point(0.5, 0.5, 0.0) + z * (x * Point(0.5, 0.5, 0.0) + y * Point(-0.5, 0.5, 0.0)));
     }
 };
 
@@ -713,10 +951,12 @@ class Node: public ItemBase, public Transformation{
     }
     
     // transformation of rays to model (local) space
-    Ray toModelSpace(Ray &ray){
-      Ray r;
+    Cone toModelSpace(Ray &ray){
+      Cone r;
       r.pos = transformTo(ray.pos);
       r.dir = transformTo(ray.pos + ray.dir) - r.pos;
+      r.tan = ray.tan;
+      r.radius = ray.radius;
       return r;
     }
     
