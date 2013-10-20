0cf1abbd6b7fe44570df11cecf4b3155ea462de8
Sean McKenna <mckennapsean@gmail.com>
2013-10-20 14:11:22 -0600

    complete adding texture.cpp implementation to library, need to fix the many bugs I'm sure

--- library/texture.cpp
+++ library/texture.cpp
@@ -28,6 +28,54 @@ class TextureFile: public Texture{
     std:vector<Color24> data;
     int width;
     int height;
+    
+    // read a line from a file to detect end of line or end of file
+    int readLine(FILE *f, int size, char *b){
+      int i;
+      for(i = 0; i < size; i++){
+        b[i] = fgetc(f);
+        if(feof(f) || b[i] == '\n' || b[i] == '\r'){
+          b[i] = '\0';
+          return i + 1;
+        }
+      }
+      return i;
+    }
+    
+    // load a PPM image file into memory
+    bool loadPPM(FILE *f, int &w, int &h, std::vector<Color24> &data){
+      
+      // variables
+      int bufferSize = 1024;
+      char buffer[bufferSize];
+      
+      // read the header
+      readLine(f, bufferSize, buffer);
+      
+      // ensure we have a proper PPM file
+      if(buffer[0] != 'P' && buffer[1] != '6')
+        return false;
+      
+      // ignore comments in header
+      readLine(f, bufferSize, buffer);
+      while(buffer[0] == '#')
+        readLine(f, bufferSize, buffer);
+      
+      // grab dimensions of PPM
+      sscanf(buffer, "%d %d", &width, &height);
+      
+      // continue skipping comments
+      readLine(f, bufferSize, buffer);
+      while(buffer[0] == '#')
+        readLine(f, bufferSize, buffer);
+      
+      // load in data for PPM image
+      data.resize(width * height);
+      fread(data.data(), sizeof(Color24), width * height, f);
+      
+      // return a succesful load from file
+      return true;
+    }
   
   public:
     
@@ -40,11 +88,61 @@ class TextureFile: public Texture{
     // load a texture from a file
     bool load(){
       
+      // reset data contents
+      data.clear();
+      width = 0;
+      height = 0;
+      
+      // open PPM file as a texture
+      FILE *f = fopen(getName(), "rb");
+      
+      // test if we have a succesful PPM image file
+      if(!f)
+        return false;
+      bool success = false;
+      success = loadPPM(f, width, height, data);
+      
+      // close PPM file
+      fclose(f);
+      return success;
     }
     
     // sample a texture for a color
     Color sample(Point &uvw){
       
+      // empty texture
+      if(width + height == 0)
+        return Color(0.0, 0.0, 0.0);
+      
+      // clamp position into texture space
+      
+      // calculate sampling positions
+      Point u = tileClamp(uvw);
+      float x = width * u.x;
+      float y = height * u.y;
+      int ix = (int) x;
+      int iy = (int) y;
+      float fx = x - ix;
+      float fy = y - iy;
+      
+      // test near edges to shift sampling positions
+      if(ix < 0)
+        ix -= (ix / width - 1.0) * width;
+      if(ix >= width)
+        ix -= (ix / width) * width;
+      int ixp = ix + 1;
+      if(ixp >= width)
+        ixp -= width;
+      if(iy < 0)
+        iy -= (iy / height - 1.0) * height;
+      if(iy >= height)
+        iy -= (iy / height) * height;
+      int iyp = iy + 1;
+      if(iyp >= height)
+        iyp -= height;
+      
+      // return the color at this point
+      return data[iy * width + ix].ToColor() * ((1.0 - fx) * (1.0 - fy)) + data[iy * width + ixp].ToColor() * (fx * ( 1.0 - fy)) + data[iyp * width + ix].ToColor() * ((1.0 - fx) * fy) + data[iyp * width + ixp].ToColor() * (fx * fy);
     }
 };
 
@@ -68,5 +166,20 @@ class TextureChecker: public Texture{
     // sample a texture for a color
     Color sample(&uvw){
       
+      // clamp position into texture space
+      Point u = tileClamp(uvw);
+      
+      // one of four cases to one of two colors, in a checkerboard fashion
+      if(u.x <= 0.5){
+        if(u.y <= 0.5)
+          return color1;
+        else
+          return color2;
+      }else{
+        if(u.y <= 0.5)
+          return color2;
+        else
+          return color1;
+      }
     }
 };
