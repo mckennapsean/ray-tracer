b356dbcf55823ba01952dc3d7d4a8c6646a6e37b
Sean McKenna <mckennapsean@gmail.com>
2013-11-26 10:44:06 -0700

    add computation of the irradiance cache, currently being output

--- ray-tracer.cpp
+++ ray-tracer.cpp
@@ -43,7 +43,7 @@ int shadowMax = 32;
 bool gammaCorr = true;
 bool globalIllum = true;
 bool irradCache = true;
-int samplesGI = 16;
+int samplesGI = 128;
 
 
 // variables for ray tracing
@@ -67,7 +67,7 @@ float Yprecalc = (1.0 / 3.0) * pow(29.0 / 6.0, 2.0);
 // setup threading
 static const int numThreads = 8;
 void rayTracing(int i);
-void rayTracingCache(int i);
+Color rayTracingCache(int i, LightList lightCache);
 
 
 // for camera ray generation
@@ -132,17 +132,24 @@ int main(){
       // calculate indirect illumination
       int cnt = 0;
       for(int i = 0; i < im.GetDataCount(); i++){
-        if(im.IsValid(i))
-          cnt++;
+        
+        // grab position on image plane
+        float px;
+        float py;
+        im.GetPosition(i, px, py);
+        
+        // get the pixel number
+        int pixel = px + py * w;
+        
+        // compute the ray tracing cache (if needs to be set)
+        if(!im.IsValid(i))
+          im.Set(i, rayTracingCache(pixel, lightCache));
+        
+        // save computation points as image for now
+        if(im.GetSubdivLevel() == 0)
+          if(im.IsValid(i))
+            img[pixel] = Color24(im.Get(i));
       }
-      cout << "size: " << im.GetDataCount() << endl;
-      cout << "valid: " << cnt << endl;
-      float x;
-      float y;
-      im.GetPosition(32, x, y);
-      cout << "(x, y): " << x << ", " << y << endl;
-      int pix = x + y * w;
-      cout << "pixel #" << pix << endl << endl;
       
       // subdivide (if necessary)
       if(subdivide)
@@ -151,16 +158,16 @@ int main(){
   }
   
   // sample test output (know irrad map done)
-  cout << "onto ray tracing!" << endl;
+  // cout << "onto ray tracing!" << endl;
   
   // start ray tracing loop (in parallel with threads)
-  thread t[numThreads];
-  for(int i = 0; i < numThreads; i++)
-    t[i] = thread(rayTracing, i);
+  // thread t[numThreads];
+  // for(int i = 0; i < numThreads; i++)
+  //   t[i] = thread(rayTracing, i);
   
   // when finished, join all threads back to main
-  for(int i = 0; i < numThreads; i++)
-    t[i].join();
+  // for(int i = 0; i < numThreads; i++)
+  //   t[i].join();
   
   // output ray-traced image & z-buffer & sample count image (if set)
   render.save("images/image.ppm");
@@ -317,6 +324,125 @@ void rayTracing(int i){
     // re-assign next pixel (naive, but works)
     pixel += numThreads;
   }
+}
+
+
+// ray tracing cache (for irradiance caching at a single pixel)
+Color rayTracingCache(int i, LightList lightCache){
+  
+  // setup random generator for anti-aliasing & depth-of-field
+  mt19937 rnd;
+  uniform_real_distribution<float> dist{0.0, 1.0};
+  
+  // number of samples
+  int s = 0;
+  
+  // establish pixel location (center)
+  float pX = i % w;
+  float pY = i / w;
+  
+  // color values to store across samples
+  Color col;
+  Color colAvg;
+  float zAvg = 0.0;
+  float rVar = 0.0;
+  float gVar = 0.0;
+  float bVar = 0.0;
+  float var = sampleThreshold;
+  float brightness = 0.0;
+  
+  // random rotation of Halton sequence on circle of confusion
+  float dcR = dist(rnd) * 2.0 * M_PI;
+  
+  // compute multi-adaptive sampling for each pixel (anti-aliasing)
+  while(s < sampleMin || (s != sampleMax && (rVar * perR > var + brightness * var || gVar * perG > var + brightness * var || bVar * perB > var + brightness * var))){
+    
+    // grab Halton sequence to shift point by on image plane
+    float dpX = centerHalton(Halton(s, 3));
+    float dpY = centerHalton(Halton(s, 2));
+    
+    // grab Halton sequence to shift point along circle of confusion
+    float dcS = sqrt(Halton(s, 2)) * camera.dof;
+    
+    // grab Halton sequence to shift point around circle of confusion
+    float dcT = Halton(s, 3) * 2.0 * M_PI;
+    
+    // compute the offset for depth of field sampling
+    Point posOffset = (*dVx * cos(dcR + dcT) + *dVy * sin(dcR + dcT)) * dcS;
+    
+    // transform ray into world space (offset by Halton seqeunce for sampling)
+    Point rayDir = cameraRay(pX + dpX, pY + dpY, posOffset);
+    Cone *ray = new Cone();
+    ray->pos = camera.pos + c->transformFrom(posOffset);
+    ray->dir = c->transformFrom(rayDir);
+    ray->radius = 0.0;
+    ray->tan = dXV->x / (2.0 * imageDistance);
+    
+    // traverse through scene DOM
+    // transform rays into model space
+    // detect ray intersections and get back HitInfo
+    HitInfo hi = HitInfo();
+    bool hit = traceRay(*ray, hi);
+    
+    // update z-buffer, if necessary
+    if(zBuffer)
+      zAvg = (zAvg * s + hi.z) / (float) (s + 1);
+    
+    // if hit, get the node's material
+    if(hit){
+      Node *n = hi.node;
+      Material *m;
+      if(n)
+        m = n->getMaterial();
+      
+      // if there is a material, shade the pixel
+      // 5-passes for reflections and refractions
+      if(m)
+        col = m->shade(*ray, hi, lightCache, bounceCount);
+      
+      // otherwise color it white (as a hit)
+      else
+        col.Set(0.929, 0.929, 0.929);
+    
+    // if we hit nothing, draw the background
+    }else{
+      Point p = Point((float) pX / w, (float) pY / h, 0.0);
+      Color b = background.sample(p);
+      col = b;
+    }
+    
+    // compute average color
+    float rAvg = (colAvg.r * s + col.r) / (float) (s + 1);
+    float gAvg = (colAvg.g * s + col.g) / (float) (s + 1);
+    float bAvg = (colAvg.b * s + col.b) / (float) (s + 1);
+    colAvg.Set(rAvg, gAvg, bAvg);
+    
+    // compute color variances
+    rVar = (rVar * s + (col.r - rAvg) * (col.r - rAvg)) / (float) (s + 1);
+    gVar = (gVar * s + (col.g - gAvg) * (col.g - gAvg)) / (float) (s + 1);
+    bVar = (bVar * s + (col.b - bAvg) * (col.b - bAvg)) / (float) (s + 1);
+    
+    // calculate and update brightness average using XYZ and Lab space
+    float Y = perR * rAvg + perG * gAvg + perB * bAvg;
+    float Y13 = Y;
+    if(Y13 > Ycutoff)
+      Y13 = pow(Y13, 1.0 / 3.0);
+    else
+      Y13 = Yprecalc * Y13 + (4.0 / 29.0);
+    brightness = (116.0 * Y13 - 16.0) / 100.0;
+    
+    // increment sample count
+    s++;
+    
+    // watch for errors at any individual sample, terminate thread if so
+    if(colAvg[0] != colAvg[0] || colAvg[1] != colAvg[1] || colAvg[2] != colAvg[2]){
+      cout << "ERROR - caching pixel " << i << " & sample " << s << endl;
+      s = sampleMax;
+    }
+  }
+  
+  // return our indirect illumination color
+  return colAvg;
 }
 
 
